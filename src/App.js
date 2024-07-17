0731966dbc3d64231b36da35679e30665e4f2c66
shadab4476 <shadabansari7726@gmail.com>
2024-07-17 21:51:21 +0530

    pages folder create Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Form from "./components/Form";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Navbar from "./components/NavBar";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
